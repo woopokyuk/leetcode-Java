89d89e7dc26aa4a5746682a6cefc47863d57752d
woopohsu <huxu1024@qq.com>
2024-10-13 01:42:32 +0800

    T64 最小路径和 动态规划

---
+++ src/main/java/org/example/vic/leetcode/T64.java
@@ -0,0 +1,30 @@
+package org.example.vic.leetcode;
+
+public class T64 {
+    /**
+     * 最小路径和
+     *
+     * @param grid 数组
+     * @return 最小路径和
+     */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[0][0];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j-1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
